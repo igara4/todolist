576602de36aaa030497a0cde38bb370714d92e67
igara4 <fifty.storm.snowman@gmail.com>
2025-02-05 11:09:00 +0900

    上げ忘れ

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Todo from './Todo';
 import { TodoData } from '../types/types';
 
-interface TodoDataProps{
+interface TodoAllDataProps{
   todoAllData:TodoData[];
 }
 
 
-const TodoList = ({todoAllData}:TodoDataProps) => {
+const TodoList = ({todoAllData}:TodoAllDataProps) => {
   return (
     <div  className="grid lg:grid-cols-3 px-4 py-4 gap-4">
         {todoAllData.map((todoData:TodoData)=>(
-        <Todo key={todoData.id}/>
+        <Todo key={todoData.id} todoData={todoData}/>
         ))}
     </div>
   )
